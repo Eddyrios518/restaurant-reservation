104a1cb4457d7d379afdf93ef14dbdbec3134630
Eddy Rios <Eddyrios101@gmail.com>
2022-08-30 13:01:02 -0700

    Implemented controllers, services, routers for reservations and tables. Created errors method

--- back-end/src/db/seeds/00-reservations.js
+++ back-end/src/db/seeds/00-reservations.js
@@ -5,7 +5,5 @@ const reservations = require("./00-reservations.json")
 exports.seed = function (knex) {
   return knex
     .raw("TRUNCATE TABLE reservations RESTART IDENTITY CASCADE")
-    .then(() => {
-      return knex("reservations").insert(reservations);
-    });
+    .then(() => knex("reservations").insert(reservations));
 };
